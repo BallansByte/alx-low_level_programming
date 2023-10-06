7ad60d4f367a10fe457c350440f420dbd818e594
Malik Ballans <kofikoo68@gmail.com>
2023-10-05 22:03:38 -0700

    codes

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - this is a main function of the program
+ * Return: always returns zero
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
