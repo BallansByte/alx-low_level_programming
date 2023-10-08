62735a7fd420d9f64013e95b842174aad3f2ae40
Malik Ballans <kofikoo68@gmail.com>
2023-10-08 10:21:12 -0700

    3rd task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ * Return: thsi gives back 0
+ */
+int main(void)
+{
+	char its_alpha;
+
+	for (its_alpha = 'a'; its_alpha <= 'z'; its_alpha++)
+	putchar(its_alpha);
+	putchar('\n');
+	return (0);
+}
