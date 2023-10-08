9b80b7dc038b46bc4a6111b6ce34a311dd1efa8d
Malik Ballans <kofikoo68@gmail.com>
2023-10-08 11:28:17 -0700

    putchar task

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-	char digit;
+	int digit;
 
-	for (digit = '0'; digit <= '9'; digit++)
+	for (digit = 0; digit < 9; digit++)
 	{
-		putchar(digit);
+		putchar(digit + '0');
 	}
 
 	putchar('\n');
