cb46ada122b05f751ca917f0841438325738b88a
Malik Ballans <kofikoo68@gmail.com>
2023-10-11 18:43:00 -0700

    task1.1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,16 @@
 #include "main.h"
 /**
+ * print_alphabet - this is to print the alphabet
 * main - check the code
 * Return: Always 0.
 */
 void print_alphabet(void)
 {
-
 	char c;
 
-	for (c ='a'; c <= 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
-
 		_putchar(c);
-	
 	}
 	_putchar('\n');
 }
