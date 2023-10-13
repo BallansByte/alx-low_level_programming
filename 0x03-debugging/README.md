5af5fda49473c9987381897350291a5e88ffb424
Malik Ballans <kofikoo68@gmail.com>
2023-10-12 19:05:09 -0700

    task 0

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+This directory cent on debugging
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * i - this is a variable
+ * void positive_or_negative(int i) is the entry point
+ * main - the main file/ entry point
+ * Return: 0 success
+ */
+void positive_or_negative(int i)
+{
+	if (i == 0)
+	{
+		printf("%dd is positive \n",  i);
+	
+	}
+	else if (i == 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is negative\n", i);
+}
