a49f32a7ed9abf17040c2ef7a89d724e263fa327
Malik Ballans <kofikoo68@gmail.com>
2023-10-19 16:18:54 -0700

    Task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+/**
+ * _strncpy - Copying a string
+ * @dest: The destination
+ * @src: The source string
+ * @n: The maximum number of bytes
+ * Return: A pointer to the resulting string dest.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for (; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
