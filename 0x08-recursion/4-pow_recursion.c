44e1142c4d04540035295f587460f5a34bfba624
Malik Ballans <kofikoo68@gmail.com>
2023-10-26 22:56:29 -0700

    Task 4.2

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,35 +1,23 @@
 #include "main.h"
-#include <unistd.h>
-#include <unistd.h>
-
 /**
- * _putchar - writes the character
- * @c: The character
- * Return: On success 1.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+  * _pow_recursion - function name
+  * @x: parameter 1
+  * @y: parameter 2
+  * Return: value of a given number passed to the function
+  */
 
-/**
- * _pow_recursion - calculates a value.
- * @x: The base number
- * @y: The exponent
- * Return: Success 0.
- */
+
 int _pow_recursion(int x, int y)
 {
-	if (y < 0)
-	{
-	return (-1);
-	}
-	else if (y == 0)
-	{
-	return (1);
-	}
-	else
-	{
-	return (x * _pow_recursion(x, y - 1));
-	}
+
+if (y < 0)
+        return (-1);
+
+else if (y == 0)
+        return (1);
+
+else
+        return (x * _pow_recursion(x, (y - 1)));
+
+return (0);
 }
