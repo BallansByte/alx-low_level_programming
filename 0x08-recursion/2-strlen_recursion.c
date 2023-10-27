c99ff1f1523765d6c1d869b4fe37765261b698d7
Malik Ballans <kofikoo68@gmail.com>
2023-10-26 21:59:36 -0700

    Task 2.1

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,26 +1,17 @@
 #include "main.h"
-#include <unistd.h>
-
 /**
- * _putchar - Writes a character
- * @c: The character
- * Return: On success, 1; on error, -1
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
- * _strlen_recursion - Calculates the length
- * @s: The string to calculate the length of
- *
- * Return: Success.
- */
+  * _strlen_recursion - main
+  * @s:parameter
+  * Return:success
+  */
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
-		return (0);
-	else
-		return (1 + _strlen_recursion(s + 1));
+	int lnt = 0;
+
+	if (*s)
+	{
+	lnt++;
+	lnt += _strlen_recursion(s + 1);
+	}
+	return (lnt);
 }
