555cb803ae6bf865d1a682358e23c6a2e33f353f
Malik Ballans <kofikoo68@gmail.com>
2023-10-29 10:26:48 -0700

    Task 3

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: 0 if successful, 1 if not
+ */
+int main(int argc, char *argv[])
+{
+	int i, result = 0;
+	char *arg;
+
+	if (argc < 2)
+	{
+	printf("0\n");
+	return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		arg = argv[i];
+		while (*arg)
+		{
+			if (*arg < '0' || *arg > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		arg++;
+		}
+	result += atoi(argv[i]);
+	}
+	printf("%d\n", result);
+	return (0);
+}
