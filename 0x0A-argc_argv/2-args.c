38164b06bc942ecc001ac278a081cb5f3988f6a8
Malik Ballans <kofikoo68@gmail.com>
2023-10-29 10:12:41 -0700

    Task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	printf("%s\n", argv[i]);
+	return (0);
+}
