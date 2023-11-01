9a51d855f69462411e4aec9445208b251ea2f7ce
Malik Ballans <kofikoo68@gmail.com>
2023-10-31 20:26:36 -0700

    Task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - Create an array of chars.
+ * @size: The size of the array to create.
+ * @c: The character to initialize the array with.
+ *
+ * Return:Success.
+* Otherwise, return a pointer to the created array.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i;
+
+	if (size == 0)
+	{
+	return (NULL);
+	}
+
+	array = (char *)malloc(size * sizeof(char));
+
+	if (array == NULL)
+	{
+	return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+	array[i] = c;
+	}
+	return (array);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+This Directory contains practice of memorry allocation
