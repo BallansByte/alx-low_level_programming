657b7bf2654ee8d7e920eb4a7ecbc574d6c5faca
Malik Ballans <kofikoo68@gmail.com>
2023-11-16 22:55:54 -0800

    Task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * pre_main_message - Prints a message before the main function is executed
+ */
+void pre_main_message(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
