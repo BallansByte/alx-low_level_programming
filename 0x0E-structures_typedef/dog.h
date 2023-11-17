d17ad828b138947f7ae386faebc148ee26dd9516
Malik Ballans <kofikoo68@gmail.com>
2023-11-16 23:46:47 -0800

    Struct1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - information
+ * @name:The Name
+ * @age:The age
+ * @owner:The animals owner
+ *
+ * Description: Longer description
+ */
+
 struct dog 
 {
 	char *name;
