3dabff6258d5e2365f4c9ab5451bcb712a7702c6
Malik Ballans <kofikoo68@gmail.com>
2023-11-22 13:17:55 -0800

    Task

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,35 +1,35 @@
+#include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "lists.h"
 
 /**
- * print_listint_safe - Prints a listint_t linked list.
- * @head: A pointer to the head..
- *
- * Return: Success Always.
+ * print_listint_safe - Prints a listint_t linked list
+ * @head: Pointer to the head.
+ * Return: The number of nodes.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *slow_ptr = head, *fast_ptr = head;
-	size_t count = 0;
+const listint_t *slow_ptr, *fast_ptr;
+size_t count = 0;
 
-	while (slow_ptr != NULL && fast_ptr != NULL && fast_ptr->next != NULL)
-	{
-	printf("[%p] %d\n", (void *)slow_ptr, slow_ptr->n);
-	count++;
+if (head == NULL)
+exit(98);
 
-	slow_ptr = slow_ptr->next;
-	fast_ptr = fast_ptr->next->next;
+slow_ptr = fast_ptr = head;
+
+while (fast_ptr != NULL && fast_ptr->next != NULL)
+{
+printf("[%p] %d\n", (void *)slow_ptr, slow_ptr->n);
+count++;
+slow_ptr = slow_ptr->next;
+fast_ptr = fast_ptr->next->next;
 
 	if (slow_ptr == fast_ptr)
 	{
-		printf("-> [%p] %d\n", (void *)slow_ptr, slow_ptr->n);
-		printf("-> [%p] %d\n", (void *)fast_ptr, fast_ptr->n);
-		printf("Linked list has a loop\n");
-		exit(98);
+	printf("-> [%p] %d\n", (void *)slow_ptr, slow_ptr->n);
+	break;
 	}
 	}
 
-	return (count);
+return (count);
 }
-
