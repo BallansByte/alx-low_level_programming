3c6270bc5a76e01b3285b6746151dbf64a24c560
BallansByte <maballans@gmail.com>
2024-05-23 07:42:50 -0700

    tASK 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,5 +1,4 @@
 #include "search_algos.h"
-
 /**
  * binary_search - performs binary search
  * @array: the integer array
@@ -8,28 +7,15 @@
  *
  * Return: the index found or -1
  */
-int binary_search(int *array, size_t size, int value)
+int main(void)
 {
-	size_t i = 0;
-	int *a = array;
+    int array[] = {10, 20, 30, 40, 50, 60};
+    size_t size = sizeof(array) / sizeof(array[0]);
+    int value = 40;
+    int result;
 
-	if (!array)
-		return (-1);
+    result = binary_search(array, size, value);
+    printf("Index of %d: %d\n", value, result);
 
-	while (size)
-	{
-		for (i = 0, printf("Searching in array: "); i < size; i++)
-			printf("%d%s", a[i], i + 1 == size ? "\n" : ", ");
-
-		i = (size - 1) / 2;
-		if (a[i] == value)
-			return ((a - array) + i);
-		else if (a[i] > value)
-			size = i;
-		else
-		{
-			a += (i + 1);
-			size -= (i + 1);
-		}
-	}
-	return (-1);
+    return (0);
+}
