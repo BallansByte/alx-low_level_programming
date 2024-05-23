1cd0109f6f02476b508fb37aa811d6044a2aa976
BallansByte <maballans@gmail.com>
2024-05-23 08:04:55 -0700

    Task 0 Amendment

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -5,12 +5,10 @@
  * @array: Pointer to the first element of the array to search in.
  * @size: Number of elements in the array.
  * @value: Value to search for.
- *
  * Return: Success.
  */
 int linear_search(int *array, size_t size, int value)
 {
-
 size_t i;
 
 if (array == NULL)
